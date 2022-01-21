eb205f800a9af4d92a2653dbb8a05f1fed6189c5
Bhavana5N <34437924+Bhavana5N@users.noreply.github.com>
2022-01-21 14:48:36 -0500

    Update Assignment_2.cpp
    New updates

--- Assignment_2.cpp
+++ Assignment_2.cpp
@@ -1,19 +1,22 @@
 /*************************************************************************
 Author: Upendra 
 
-This program will demonstrate a variety of tools that can be used when
-reading a data stream from user input
+This program will help the Oiler Bank get their ATM up and running.
 
 Input: 
-		Various inputs from the user, indicated by prompts
+		[D] Deposit
+        [W] Withdraw
+        [B] Balance
+        [X] Done
 
 Output: 
-		Results of each example are written to the console
+		All the banking options from the input can be performed.
 
 *************************************************************************/
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -21,69 +24,64 @@ int main()
     float init_bal;
     string char_sel;
     float dep_amt;
-    float total_bal;
     float withdrawl_amt;
     float current_bal;
 
-
-	//get function
-	//Example Input:  A 25
-    init_bal = 1000;
-    
-    
+    init_bal = 1000; 
     cout << "Current balance $" << init_bal << endl;
-    cout<< "*** Oiler Banking *** \n";
-    cout<< " [D] Deposit \n";
-    cout<< " [W] Withdraw \n";
-    cout<< " [B] Balance \n";
-    cout<< " [X] Done \n";
-        
     
-   /* if (    char_sel == "D" 
-            ||  char_sel == "W"
-            ||  char_sel == "B"
-            ||  char_sel == "X")
-
-         {  */ 
-
     current_bal = init_bal;
+    cout<< fixed<< setprecision(2);
+
+    /* While loop is used to run the valid options until X or invalid value is entered to exit the loop */
     while (1)
-    
-    {
+    { 
+        cout<< "\n*** Oiler Banking *** "<< endl;
+        cout<< " [D] Deposit "<< endl;
+        cout<< " [W] Withdraw "<< endl;
+        cout<< " [B] Balance "<< endl;
+        cout<< " [X] Done "<< endl;
         cout<<"\nEnter valid input from above: ";
         cin>> char_sel;
+        /* Deposit amount should be within givin limits */
         if (char_sel == "D")
         {
             cout<< "Please enter the value between $0.01 and $5000: ";
             cin>>dep_amt;
-            if(dep_amt >= 0.01
-                && dep_amt <= 5000)
+            if  (   dep_amt >= 0.01
+                &&  dep_amt <= 5000
+                )
             {
                 current_bal = current_bal + dep_amt;
-                cout<<"New balance $"<< current_bal<< endl;
+                cout<< "New balance is $"<< current_bal<< endl;
             }
             else
             {
-                cout<<"An invalid amount has been entered";
+                cout<<"An invalid amount has been entered"<< endl;
             }
         }
+        /* Withdrawl amount has conditions like positive value, multiple of 10's and within current balance limit */
         else if (char_sel == "W")
         {
             cout<< "The value must be a positive number, a multiple of 10 and less than or equal to the balance : ";
-            cin>>withdrawl_amt;
-            if( withdrawl_amt > 0
-            && remainder (withdrawl_amt,10.0) == 0
-            && withdrawl_amt <= current_bal )
+            cin>> withdrawl_amt;
+            if(withdrawl_amt <= 0)
             {
-                current_bal = current_bal - withdrawl_amt;
-                cout<<"new balance $"<< current_bal<< endl;
+                cout<< "Amount not a positive number"<< endl;
+            }
+            else if(remainder (withdrawl_amt,10.0) != 0)
+            {
+                cout<< "Amount is not a multiple of 10"<< endl;
+            }
+            else if(withdrawl_amt >= current_bal)
+            {
+                cout<< "Insufficiant funds in your account"<< endl;
             }
             else
             {
-                cout<<"Amount not a positive number or amount not a multiple of 10 or insufficient funds depending on the problem";
+                current_bal = current_bal - withdrawl_amt;
+                cout<< "New balance is $"<< current_bal<< endl;
             }
-            
-
         }
         else if (char_sel == "B")
         {
@@ -91,12 +89,12 @@ int main()
         }
         else if (char_sel == "X")
         {
-            cout<<" Your transaction is done. Thank You";
+            cout<< "Thank You for using Oiler Bank";
             break;
         }
         else
         {
-            cout<<"Valid choice is not entered. Thank You";
+            cout<< "Valid choice is not entered. Thank You";
             break;
         }
         
